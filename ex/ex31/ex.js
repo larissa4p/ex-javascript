2354e78fa6d18eb8486c384c1537000ae11ee7db
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-05-08 19:41:07 -0300

    ex

---
+++ ex/ex31/ex.js
@@ -0,0 +1,13 @@
+let num = [44, 23, 56, 77, 11]
+
+let num1 = [21, 23, 41,]
+
+function verificar(v){
+    if(v.length >= 5) {
+        console.log("Muitos elementos")
+    } else {
+        console.log("Poucos elementos")
+    }
+}
+
+verificar(num1)
---
+++ ex/ex32/ex.js
@@ -0,0 +1,5 @@
+let contando = [4, 7, 20, 15, 10]
+
+for(let c = 0; c <= contando.length; c++) {
+    console.log(contando[c])
+}
