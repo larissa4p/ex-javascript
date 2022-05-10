4de063fcf69bc014f21180e3b5a6f975557aadc4
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-05-10 16:26:12 -0300

    +

---
+++ metodos/01_metodos/script.js
@@ -0,0 +1,11 @@
+const cachorro = {
+    uivar: function() {
+        console.log("Auuuuuuuuu")
+    },
+    rosnar: function(){
+        console.log("grrrrrrr")
+    }
+}
+
+cachorro.uivar()    
+cachorro.rosnar()
---
+++ metodos/02_metodos_2/script.js
@@ -0,0 +1,19 @@
+const cachorro = {
+    raca: 'SRD',
+    uivar: function() {
+        console.log("Auuuuuuuuu")
+    },
+    rosnar: function(){
+        console.log("grrrrrrr")
+    },
+    setRaca: function(raca){
+        this.raca = raca
+    },
+    getRaca: function() {
+        return "A raça é " + this.raca
+    }
+}
+console.log(cachorro.raca)
+cachorro.setRaca('Pastor Alemão')
+console.log(cachorro.raca)
+console.log(cachorro.getRaca())
