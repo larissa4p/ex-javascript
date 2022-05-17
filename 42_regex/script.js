39bd761cfea440a1cd846a50c187b5090a0c9ade
Larissa <98964013+larissa4p@users.noreply.github.com>
2022-05-17 14:27:45 -0300

    *

---
+++ 42_regex/script.js
@@ -0,0 +1,15 @@
+const reg1 = new RegExp('bola');
+
+console.log(reg1.test("tem bola?"))
+console.log(reg1.test("Não tem"));
+
+const reg2 = /bola/;
+
+let text = 'Tem bola na cesta';
+
+console.log(reg2.test("tem bola?"))
+console.log(reg2.test("Não tem"));
+console.log(reg2.test(text));
+
+console.log(/quadrado/.test("Oonde tem um quadrado"));
+console.log(/quadrado/.test("123123123quadrado323123123"));
